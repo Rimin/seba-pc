90dbf22c392455926729887d05f1717361128c5a
vlzf <1470064691@qq.com>
2018-07-13 13:36:18 +0800

    7/13 13.36

--- src/common/lang/en.js
+++ src/common/lang/en.js
@@ -1,4 +1,96 @@
 export const m = {
   about: 'ABOUT US',
-  contact: 'CONTACT'
+  contact: 'CONTACT',
+  "Order": 'MORE',
+  'Order_Title': 'YOUR ORDER',
+  'SelectColor': 'COLOR',   // 颜色选择
+  
+  'Continue': 'CONTINUE',
+  'Back': 'BACK',
+  'Confirm': 'COMFIRM',
+  'Upload_Images': 'UPLOAD IMAGES',
+
+  'HIGH_CARBON': {    // HIGH CARBON 品牌的部件名
+    'Main_Upper': 'Main Upper(PU)',
+    'Toe_Part': 'Toe Part(PU)',
+    'Toe_Strap': 'Toe Strap(PU)',
+    'Eyelet_Part': 'Eyelet Part(PU)',
+    'Tongue': 'Tongue(PU)',
+    'Collar': 'Collar(PU)',
+
+    'Toe_Cap': 'Toe Cap',
+    'Abrasive_Pad': 'Abrasive Pad',
+    'Side_Window': 'Side Window',
+    'Double_Strap': 'Double Strap',
+    'Spider_Buckle': 'Spider Buckle',
+    'Cuff_Button': 'Cuff Button',
+    'CUFF': 'CUFF',
+    'Lace': 'Lace',
+    'Flat_Frame': 'Flat Frame',
+    'Wheels': 'Wheels',
+  },
+ 
+  'RIX': {     // RIX 品牌的部件名
+    'Collar': 'Collar(PU)',
+    'Tongue': 'Tongue(PU)',
+    'Collar_Strap': 'Collar strap (Lower)',
+    'Collar_Strap': 'Collar strap (Upper)',
+    'Eyelet_Part': 'Eyelet Part(PU)',
+    'Toe_Strap': 'Toe Strap(PU)',
+    'Toe_Part': 'Toe Part(PU)',
+    'Main_Upper': 'Main Upper(PU)',
+    'Double_Strap': 'Double Strap',
+    'Spider_Buckle': 'Spider Buckle',
+    'Abrasive_Pad': 'Abrasive Pad',
+    'CUFF': 'CUFF',
+    'Lace': 'Lace',
+    'Flat_Frame': 'Flat Frame',
+    'Wheels': 'Wheels',
+  },
+
+  'Produce': {   // 订单产品规格
+    'Title': 'DETAILS',
+    'Left_Size': 'LEFT SIZE',
+    'Right_Size': 'RIGHT SIZE',
+    'Frame_Size': 'FRAME SIZE',
+    'Wheel_Size': 'WHEEL SIZE',
+  },
+
+  'Customised_Logo': {   // 自定义刺绣
+    'Title': 'CUSTOMISED LOGO',
+    'Please_Input': 'Please type here',
+  },
+
+  'Font_Family': {   // 字体
+    'arial': 'Arial',
+    'cambria': 'Cambria',
+    'calibra': 'Calibra',
+    'century': 'Century'
+  },
+
+  'Personal_Information': {     // 订单个人信息
+    'Title': 'PLEASE FILL OUT THE FOLLOWING FORM.',
+    'First_Name': 'FIRST NAME',
+    'Last_Name': 'LAST NAME',
+    'Phone_Number': 'PHONE NUMBER',
+    'Email': 'EMAIL',
+    'Post_Code': 'POST CODE',
+    'Address': 'ADDRESS',
+    'Please_Perfect_Address': 'Please perfect the address',
+    'SEBA_Store': 'SEBA STORE&ADDRESS',
+    'Special_Requests': 'SPECIAL REQUESTS',
+  },
+
+  'Area': {   // 区域
+    'CHN': 'CHN',   
+    'HK': 'HK',
+    'TWN': 'TWN',
+    'EUP': 'EUR',
+    'USA': 'USA',
+    'RUS': 'RUS',
+    'SG': 'SG',
+    'THA': 'THA',
+    'MAS': 'MAS',
+    'Other': 'Other',
+  }
 }
--- src/common/lang/zh.js
+++ src/common/lang/zh.js
@@ -1,4 +1,96 @@
 export const m = {
   about: '关于我们',
-  contact: '联系我们'
+  contact: '联系我们',
+  "Order": '更多定制',
+  'Order_Title': '您的订单信息',
+  'SelectColor': '颜色选择',
+  
+  'Continue': '下一步',
+  'Back': '返回',
+  'Confirm': '确认下单',
+  'Upload_Images': '上传图片',
+  
+  'HIGH_CARBON': {    // HIGH CARBON 品牌的部件名
+    'Main_Upper': '大身内外/下包铮',
+    'Toe_Part': '鞋头/大身饰花/上包铮',
+    'Toe_Strap': '前能量带',
+    'Eyelet_Part': '眼排',
+    'Tongue': '领口饰花/利仔饰花',
+    'Collar': '领口',
+
+    'Toe_Cap': '鞋头壳',
+    'Abrasive_Pad': '防磨片',
+    'Side_Window': '透气窗',
+    'Double_Strap': '脚背护带/能量带',
+    'Spider_Buckle': '脚背护带芭扣',
+    'Cuff_Button': '穿钉冬茹头',
+    'CUFF': '后铮CUFF',
+    'Lace': '鞋带',
+    'Flat_Frame': '刀架',
+    'Wheels': '轮子',
+  },
+
+  'RIX': {     // RIX 品牌的部件名
+    'Collar': '鞋帮',
+    'Tongue': '鞋舌',
+    'Collar_Strap': '鞋帮能量带下片',
+    'Collar_Strap': '鞋帮能量带上片',
+    'Eyelet_Part': '眼排',
+    'Toe_Strap': '前能量带',
+    'Toe_Part': '鞋头',
+    'Main_Upper': '大身',
+    'Double_Strap': '脚背能量带',
+    'Spider_Buckle': '脚背能量带芭扣',
+    'Abrasive_Pad': '防磨片',
+    'CUFF': '三角CUFF',
+    'Lace': '鞋带',
+    'Flat_Frame': '刀架',
+    'Wheels': '轮子',
+  },
+
+  'Produce': {   // 订单产品规格
+    'Title': 'DETAILS',
+    'Left_Size': '左脚码数',
+    'Right_Size': '右脚码数',
+    'Frame_Size': '刀架尺寸',
+    'Wheel_Size': 'SI轮子尺寸',
+  },
+
+  'Customised_Logo': {   // 自定义刺绣
+    'Title': '自定义刺绣',
+    'Please_Input': '点击添加文字',
+  },
+
+  'Font_Family': {   // 字体
+    'arial': 'Arial',
+    'cambria': 'Cambria',
+    'calibra': 'Calibra',
+    'century': 'Century'
+  },
+
+  'Personal_Information': {     // 订单个人信息
+    'Title': '个人信息',
+    'First_Name': '名',
+    'Last_Name': '姓',
+    'Phone_Number': '电话',
+    'Email': '邮箱',
+    'Post_Code': '邮编',
+    'Address': '地址',
+    'Please_Perfect_Address': '请完善地址',
+    'SEBA_Store': 'SEBA专卖店及地址',
+    'Special_Requests': '特殊要求',
+  },
+
+  'Area': {   // 区域
+    'CHN': '中国',
+    'HK': '香港',
+    'TWN': '台湾',
+    'EUP': '欧洲',
+    'USA': '美国',
+    'RUS': '俄罗斯',
+    'SG': '新加坡',
+    'THA': '泰国',
+    'MAS': '马来西亚',
+    'Other': '其他',
+  }
 }
