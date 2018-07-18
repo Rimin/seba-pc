08b596e7c429e7ee30972e27db8cb3c9508b0e1d
vlzf <1470064691@qq.com>
2018-07-18 16:24:22 +0800

    7/18 16.24

---
+++ src/components/embroidery/embroidery.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="embroidery-box">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+.embroidery-box {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
+
