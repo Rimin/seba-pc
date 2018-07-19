b2c855aed4264b979c196dd29ee55c85e197e264
vlzf <1470064691@qq.com>
2018-07-20 00:38:27 +0800

    7/20 0.38

--- src/common/js/fn.js
+++ src/common/js/fn.js
@@ -1,13 +1,13 @@
 export const imageFileToBase64 = function (file) {
   return new Promise((resolve, reject) => {
     if (!file) {
-      reject(Error('未选择图片'))
+      reject(Error(1)) // '未选择图片'
     } else if (typeof FileReader === 'undefined') {
-      reject(Error('你的浏览版本过低！'))
+      reject(Error(2)) // '你的浏览版本过低！'
     } else if (!/image\/\w+/.test(file.type)) {
-      reject(Error('非图片'))
+      reject(Error(3)) // '非图片'
     } else if (file.size > 3145728) {
-      reject(Error('图片不得超过 3MB !'))
+      reject(Error(4)) // '图片不得超过 3MB !'
     } else {
       var reader = new FileReader()
       reader.onload = function () {
